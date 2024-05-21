6ee172a9a4f46dff4a8cfba32f2c4725dc5380c8
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-21 19:03:09 +1200

    Remove LinkedIn comment in home page

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
 
         <h2>Let&apos;s Connect</h2>
 
-        <p>I&apos;d love to connect and discuss potential collaborations or opportunities. Feel free to reach out to me via email or connect with me on LinkedIn. Don&apos;t forget to check out my portfolio to see some of my latest projects.</p>
+        <p>I&apos;d love to connect and discuss potential collaborations or opportunities. Feel free to reach out to me via email. Don&apos;t forget to check out my portfolio to see some of my latest projects.</p>
       </article>
     </section>
   );
