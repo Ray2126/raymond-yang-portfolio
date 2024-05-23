9774f0a4f3f611d170b442004e4b39de7ef963f2
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-23 19:46:56 +1200

    Add padding around main view

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="max-w-screen-md m-auto">
+      <body className="max-w-screen-md m-auto p-8">
         <Navbar />
-        <main className="mt-24">{children}</main>
+        <main className="mt-16">{children}</main>
         <Footer />
       </body>
     </html>
