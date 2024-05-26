ec20cb02015630821c508d572dbc4c4c2fb17820
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-27 10:52:33 +1200

    Add analytics for visitors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Raymond Yang",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
           <main className="mt-16">{children}</main>
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
