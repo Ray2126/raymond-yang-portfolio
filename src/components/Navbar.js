07c9c8280e893a38d669cb56237bfa112a130cea
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-27 22:25:08 +1200

    Add CV

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 export default function Navbar() {
   const [ mobileMenuIsOpen, setMobileMenuIsOpen ] = useState(false);
@@ -25,6 +26,9 @@ export default function Navbar() {
       <div className="hidden md:flex justify-between space-x-8">
         <Link className="nav-link" href="/blog">Blog</Link>
         <Link className="nav-link" href="/projects">Projects</Link>
+        <Link className="nav-link flex items-center" href="/raymond_yang_cv.pdf" target="_blank" rel="noopener noreferrer">
+          CV <MdOutlineFileDownload className="h-6 w-6" />
+        </Link>
       </div>
       <div className="md:hidden">
         <button onClick={() => setMobileMenuIsOpen(true)}>
@@ -36,10 +40,13 @@ export default function Navbar() {
             <button className="flex justify-end w-full">
               <RxCross1 className="h-7 w-7" onClick={() => setMobileMenuIsOpen(false)} />
             </button>
-            <div className="text-2xl space-y-2 mt-8">
+            <div className="text-2xl space-y-2 mt-8 flex flex-col">
               <button className="nav-link" onClick={() => handleLinkClick('/')}>Home</button>
               <button className="nav-link" onClick={() => handleLinkClick('/blog')}>Blog</button>
               <button className="nav-link" onClick={() => handleLinkClick('/projects')}>Projects</button>
+              <Link className="nav-link flex items-center justify-center" href="/raymond_yang_cv.pdf" target="_blank" rel="noopener noreferrer">
+                CV <MdOutlineFileDownload className="h-6 w-6" />
+              </Link>
             </div>
           </div>
           : null
