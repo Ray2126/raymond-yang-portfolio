dc058542b4341dd9a72107555c79b2b912b28427
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-28 21:24:52 +1200

    Add aria labels to the links in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ export default function Footer() {
     <footer className="flex justify-between py-12 px-2 md:px-4 mt-auto">
       <div>&copy; Raymond Yang</div>
       <div className="flex justify-between space-x-4 md:space-x-8">
-        <Link className="styled-link-tertiary" href="mailto:raymondyang@outlook.co.nz">
+        <Link className="styled-link-tertiary" href="mailto:raymondyang@outlook.co.nz" aria-label="Mail to email address: raymondyang@outlook.co.nz">
           <MdEmail className="w-7 h-7" />
         </Link>
-        <Link className="styled-link" style={{ color: '#e8eaea' }} href="https://github.com/Ray2126" target="_blank">
+        <Link className="styled-link" style={{ color: '#e8eaea' }} href="https://github.com/Ray2126" target="_blank" aria-label="Go to Raymond's GitHub profile">
           <FaGithub className="w-6 h-6" />
         </Link>
-        <Link className="styled-link" style={{ color: '#0072b1' }} href="https://www.linkedin.com/in/raymond-yang-96365419a/" target="_blank">
+        <Link className="styled-link" style={{ color: '#0072b1' }} href="https://www.linkedin.com/in/raymond-yang-96365419a/" target="_blank" aria-label="Go to Raymond's LinkedIn profile">
           <FaLinkedin className="w-6 h-6" />
         </Link>
       </div>
