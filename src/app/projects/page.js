a35afe094013ffaeffd10ed95f21c6772bac2339
Raymond Yang <raymond.yang@centrapay.com>
2025-06-18 20:52:32 +1200

    Add iSQROLL project

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaGooglePlay, FaGithub } from "react-icons/fa";
+import { FaGooglePlay, FaGithub, FaAppStore } from "react-icons/fa";
 import { LuExternalLink } from "react-icons/lu";
 
 const projects = [
+  {
+    name: 'iSQROLL',
+    description: 'iSQROLL is a marketplace app built by Kiwis, for Kiwis. A community platform that provides a safe buying and selling environment with easy communication between traders. Hired as a freelancer. Built the backend, frontend, Android and iOS apps, and handled deployments. Liased with clients to set requirements and expectations.',
+    links: [
+      { icon: LuExternalLink, text: 'Demo', href: 'https://isqroll.co.nz/home' },
+      { icon: FaGooglePlay, text: 'Google Play', href: 'https://play.google.com/store/apps/details?id=com.isqroll&hl=en' },
+      { icon: FaAppStore, text: 'App Store', href: 'https://apps.apple.com/nz/app/isqroll/id6479675690' },
+    ],
+    demoIcon: LuExternalLink,
+    demoText: 'Demo',
+    demoLink: 'https://www.capitalise.space/projects/654d7ef11d6ecfc86ae1e10c',
+    githubLink: 'https://github.com/uoa-compsci399-s2-2023/capstone-project-team-9',
+    technologies: ['Node.js', 'React Native', 'Metro', 'Vue.js', 'Microsoft Azure'],
+    image: '/isqroll.png',
+  },
   {
     name: 'Tabs',
     description: 'Tabs is a mobile app that uses OCR technology to convert handwritten notes into digital text, streamlining file organization and task management by integrating with Microsoft APIs. Developed by a team of six for a final year Computer Science project, it received the Excellence award from the Computer Science department.',
@@ -41,14 +56,14 @@ export default function Projects() {
       <div className="space-y-12">
         {projects.map(project => (
           <div key={project.name} className="flex space-x-8">
-            <Image className="hidden md:block" src={project.image} alt={project.name} width={800} height={500} quality={100} />
+            <Image className="hidden md:block max-w-48" src={project.image} alt={project.name} width={800} height={500} quality={100} />
             <div className="border-2 rounded-2xl border-color-primary flex items-center md:space-x-4 px-2 pt-2">
               <div className="flex flex-col h-full space-y-4 my-4">
                 <div className="flex flex-col items-center md:flex-row md:space-x-4 space-y-4 md:space-y-0">
                   <h2>{project.name}</h2>
                   <div className="flex space-x-1 sm:space-x-4">
                     {project.links.map(link => (
-                      <Link className="btn styled-link" target="_blank" href={link.href}>
+                      <Link key={link.text} className="btn styled-link" target="_blank" href={link.href}>
                         <span className="mr-1">{link.icon()}</span>
                         <span>{link.text}</span>
                       </Link>
