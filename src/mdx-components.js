d7a42ef1e64ef3ca1d24dcba9fd3cc9b6b3a2ffd
Raymond Yang <raymond.yang@centrapay.com>
2025-06-21 22:28:03 +1200

    Add blog post - drag and drop

--- src/mdx-components.js
+++ src/mdx-components.js
@@ -4,6 +4,15 @@ export function useMDXComponents(components) {
     pre: props => (
       <pre {...props} className="text-sm rounded-2xl border-2 border-color-primary px-2 py-2" />
     ),
+    a: props => (
+      <a {...props} className="styled-link-tertiary" />
+    ),
+    ul: props => (
+      <ul {...props} className="list-disc list-inside space-y-1 pl-4" />
+    ),
+    li: props => (
+      <li {...props} className="text-base leading-relaxed" />
+    ),
     ...components,
   }
 }
